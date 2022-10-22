f681be74e7108dbef8edcba0b0bb38e4a205fea1
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-10-22 14:34:42 +0200

    Installing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was created by Karolina and is{" "}
-          <a
-            href="https://github.com/Karolinaa12/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          <p>
+            This project was created by Karolina and is{" "}
+            <a
+              href="https://github.com/Karolinaa12/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
