59b97537899a32c9c22e28e204f334d83a9ee3ef
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-10-22 16:03:22 +0200

    Added styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Type a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,8 +30,13 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Clouds" />
-          21 °C
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Clouds"
+          />
+
+          <span className="temperature">21</span>
+          <span className="units">°C</span>
         </div>
         <div className="col-6">
           <ul>
