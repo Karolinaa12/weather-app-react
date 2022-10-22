26b5269b8f4da4975c3ef0ca84e6a8ab8312491e
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-10-22 14:41:30 +0200

    Added html structure

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,22 @@ export default function Weather() {
   return (
     <div className="Weather">
       <h1>Lisbon</h1>
+      <ul>
+        <li>Last updated at Saturday 14:35</li>
+        <li>Clouds</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="Clouds" />
+          21 °C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 73%</li>
+            <li>Wind: 17km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
