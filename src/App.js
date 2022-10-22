cf53f4f062fc79336248cf4457cd4c71a9cb8cc2
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-10-22 13:58:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ export default function App() {
       <footer>
         <p>
           This project was created by Karolina and is{" "}
-          <a href="https://github.com/Karolinaa12/weather-app-react">
+          <a
+            href="https://github.com/Karolinaa12/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
             open-sourced on Github
           </a>
         </p>
